5d5199ca97222563becc30fef66abda46eedf207
Reynaldo Ferdian Saputra <rferdian516@gmail.com>
2021-12-01 17:51:00 +0700

    initial folder

---
+++ lib/auth/view/boarding_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BoardingPage extends StatefulWidget {
+
+  @override
+  _BoardingPageState createState() => _BoardingPageState();
+}
+
+class _BoardingPageState extends State<BoardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
